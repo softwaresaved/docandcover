5af9aef971e194d6de1601a165cf309ae3dc32cd
Robin Long <r.long@lancaster.ac.uk>
2017-03-29 15:12:34 +0100

    Updated to allow any git repo.

--- docandcover.py
+++ docandcover.py
@@ -1,8 +1,25 @@
+#input_var='https://github.com/softwaresaved/docandcover/'
+input_var='https://github.com/Axelrod-Python/Axelrod'
+
+import os
+import shutil
+from pygit2 import clone_repository
+try:
+    shutil.rmtree('tmp-repo')
+except:
+    pass
+
+try:
+    repo = clone_repository(input_var,'tmp-repo')
+except:
+    pass
+
+
 from fileListGetter import fileListGetter
 from functionDetailGetter import functionDetailGetter
 
 #fileLists = fileListGetter('../snippets/tmp-repo/')
-fileLists = fileListGetter('.')
+fileLists = fileListGetter('tmp-repo')
 
 for i in functionDetailGetter(fileLists):
     print i
