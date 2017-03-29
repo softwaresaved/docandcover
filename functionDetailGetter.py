026fc8cef8a6068c2c850b38e8de09d96d3d1911
Robin Long <r.long@lancaster.ac.uk>
2017-03-29 14:05:23 +0100

    added functionDetailGetter.py

---
+++ functionDetailGetter.py
@@ -0,0 +1,89 @@
+fileLists = [['fileListGetter.py', 'python']]
+
+import re
+
+def functionDetailGetter(fileLists):
+    """
+    Function to get list of fuctions in file and associated outputs.
+    Input: fileLists : List of Lists. Lists are a pair of strings, format
+    filename, language type.
+
+        Output is functionDetails:
+    [
+    string: docandcover.py, # file
+    string: fileSearch, # function name
+    string: [ filename, filetype], # list of inputs
+    string: [ my_output], # list of outputs
+    string: snippet # documention or comments
+    ]
+    """
+    functionDetails = []
+    
+    for fileList in fileLists:
+        filename = fileList[0]
+        languageType = fileList[1]
+
+        if languageType == 'python':
+            pythonFunctionDetailsGetter(filename, functionDetails)
+        else:
+            continue
+        
+    return functionDetails
+
+
+def pythonFunctionDetailsGetter(filename, functionDetails):
+    """ 
+    Function to search through python files and extract functions, inputs,
+    outputs and comments.  These are appened to the list passed to it
+    Input: filename (and path) as string, functionDetails as list
+    OUtput: none, modifies input.
+
+    functionDetails layout:
+    [
+    string: docandcover.py, # file
+    string: fileSearch, # function name
+    string: [ filename, filetype], # list of inputs
+    string: [ my_output], # list of outputs
+    string: snippet # documention or comments
+    ]
+    """
+
+    #    file = open(filename)
+    f = open(filename, 'r')
+    lineContainingFunction = ''
+    linesIncludingComments  = ''
+    for line in f.read().split('\n'):
+        if re.search('def', line):
+            lineContainingFunction = line
+
+            cleanedLine = re.sub('.*def\ ','', lineContainingFunction)
+            if cleanedLine:
+                functionNameMatch = re.search('([a-zA-Z0-9_-]+)\(', cleanedLine)
+                if functionNameMatch.groups > 1:
+                    functionName = functionNameMatch.group(1)
+                
+            functionInputsMatch = re.search('\((.+)\)', cleanedLine)
+            if functionInputsMatch:
+                functionInputs = [parameter.strip() for parameter in (functionInputsMatch.group(1)).split(',')]
+            else:
+                functionInputs = []
+            
+            linesIncludingComments = ''
+        linesIncludingComments += line
+
+        if re.search('\"\"\".*\"\"\"', linesIncludingComments):
+            #We are at the end of a comment, process and clean data
+     
+            functionDetail = [filename, functionName, functionInputs, [], linesIncludingComments]
+            functionDetails.append(functionDetail)
+            linesIncludingComments = ''
+
+    f.close()
+
+    return
+
+def bar():
+    return 1
+
+
+print functionDetailGetter(fileLists)
