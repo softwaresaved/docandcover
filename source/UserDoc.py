c7b1e8c5ebbd6e5f9babc16404c11749260c5e13
M. H. Beals <m.h.beals@lboro.ac.uk>
2017-03-29 15:00:25 +0100

    Re-introduce imports

--- source/UserDoc.py
+++ source/UserDoc.py
@@ -1,3 +1,6 @@
+import fileinput
+import re
+
 lists = [['fileListGetter.py', 'fileListGetter', ['directory', '_nsns'], [], 'def fileListGetter(directory, _nsns):    """    Function to get list of files and language types     Inputs:  directory: Stirng containing path to search for files in.    Outputs:  List of Lists.  Lists are of format, [filename, language type]    """'], ['fileListGetter.py', 'getLanguageType', ['file_extention'], [], 'def getLanguageType(file_extention):    """    Function to assign language type based on file extention.    Input: file_extention: String that lists file type.    Output: languageType: string listsing identified language type.    """'], ['fileListGetter.py', 'printFileLists', ['fileLists'], [], 'def printFileLists(fileLists):    """    Function to print out the contents of fileLists    Main use is debugging    """']]
 comments = ""
 comment = ""
